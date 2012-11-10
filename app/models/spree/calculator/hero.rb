90cb81f607799cc1e274e0f9d5d016e70cb679a4
ideevo <github@ideevo.com>
2012-11-10 17:13:13 +0100

    Added Custom Shipping calculator

---
+++ app/models/spree/calculator/hero.rb
@@ -0,0 +1,36 @@
+module Spree
+  class Calculator::Hero < Calculator
+
+    preference :first_item,             :decimal, :default => 4.3
+    preference :additional_item,        :decimal, :default => 4.3
+    preference :free_after_items_count, :integer, :default => 9
+
+    attr_accessible :preferred_first_item, :preferred_additional_item, :preferred_free_after_items_count
+
+    def self.description
+      'Kleiderbügel Frachtrechner'
+    end
+
+    def self.available?(object)
+      true
+    end
+
+    def compute(object)
+      items_count = object.line_items.map(&:quantity).sum
+      if items_count > self.preferred_free_after_items_count.to_i
+        return 0.0
+      else
+        sum = 0
+        items_count.times do |i|
+          if i == 0
+            sum += self.preferred_first_item.to_f
+          else
+            sum += self.preferred_additional_item.to_f
+          end
+        end
+        sum
+      end
+    end
+
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,10 @@ module Ideevo
       end
     end
 
+    initializer 'spree.register.calculators' do |app|
+      app.config.spree.calculators.shipping_methods << Spree::Calculator::Hero
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
