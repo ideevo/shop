e7a4eff6bedf22aa499ede305ebd7255b52428b0
ideevo <github@ideevo.com>
2012-11-10 17:45:32 +0100

    Admin CSS tweaks

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,6 +1,7 @@
 Spree::AppConfiguration.class_eval do
   preference :default_locale, :string, default: :de
   preference :logo, :string, default: '/logos/ideevo/black170x40.png'
+  preference :admin_interface_logo, :string, default: '/assets/logos/ideevo/white200x46.png'
   preference :auto_capture, :boolean, default: true
   preference :mails_from, :string, default: 'noreply@ideevo.com'
   preference :max_level_in_taxons_menu, :integer, default: 3
